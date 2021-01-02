46f57965d468da517e949f5db6f8ff56f8382bd9
nightblooda <himanshux7x7x7@gmail.com>
2021-01-02 20:02:02 +0530

    commit

--- src/context.js
+++ src/context.js
@@ -10,9 +10,13 @@ export class Provider extends Component {
         heading : "Top 10 Tracks"
     }
 
-    componentDidCatch(){
-        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_AAP_MM_KEY}`)
-        .then(res => console.log(res.data))
+    componentDidMount(){
+        axios
+        .get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+        .then(res => {
+            //console.log(res.data)
+            this.setState({ track_list : res.data.message.body.track_list });
+        })
         .catch(err => console.log(err))
     }
     render() {
