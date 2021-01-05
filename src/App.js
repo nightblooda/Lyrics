2e70c64f70354f3bac10715b98005641d82e0841
nightblooda <himanshux7x7x7@gmail.com>
2021-01-05 12:57:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
 import Index from './components/layouts/Index'
+import Lyrics from './components/tracks/Lyrics'
 import { Provider } from './context'
 
 export class App extends Component {
@@ -14,6 +15,7 @@ export class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
           </React.Fragment>
